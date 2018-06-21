78edf5d3239cf322b698678c814ae3ee5b044d36
Sean Oughton <seanoughton@gmail.com>
2018-06-21 00:07:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ function destructivelyUpdateDriverWithKeyAndValue(obj, key, value) {
 }
 
 function deleteFromDriverByKey(obj, key) {
-  //deletes `key` from a clone of driver and returns the new driver (it is non-destructive)
   const newObj = { ...obj };
   //console.log(key);
   delete newObj.key
